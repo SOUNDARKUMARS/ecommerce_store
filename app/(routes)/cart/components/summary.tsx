02e51f7dff02ac01ac2185217ac049a0c3ed3d82
soundarkumar <soundarkumarsaravanan@gmail.com>
2023-07-27 17:21:22 -0700

    todo

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -16,6 +16,7 @@ const Summary = () => {
   const [customerName, setCustomerName] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [address, setAddress] = useState("");
+  const [description, setdescription] = useState("");
 
   const total = items.reduce((tot, item) => {
     return tot + Number(item.price);
@@ -24,8 +25,22 @@ const Summary = () => {
   const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
+   
+    const cartItemsContent = items
+      .map((item) => `${item.name} - ${item.price}`)
+      .join("\n");
+
+
+    const templateParams = {
+      customerName,
+      phoneNumber,
+      address,
+      cartItems: cartItemsContent,
+      description
+    };
+
     emailjs
-      .sendForm("service_fufu9yl", "template_uv6wfby", form.current!, "0k1ikOz8LCyPMcJiE") // Replace with your email.js parameters
+      .send("service_fufu9yl", "template_uv6wfby", templateParams, "0k1ikOz8LCyPMcJiE")
       .then(
         (result) => {
           console.log(result.text);
@@ -36,6 +51,7 @@ const Summary = () => {
       );
     e.currentTarget.reset();
     toast.success("Order Placed! Item on the way...");
+    removeAll()
   };
 
   return (
@@ -48,38 +64,67 @@ const Summary = () => {
         </div>
       </div>
       <div>
-            <form ref={form} onSubmit={sendEmail} className="fform">
-                
-                <div className="fflex mt-2">
-                    <label>
-                        <input required type="text" name='customerName' className="iinput w-full"/>
-                        <span>Username</span>
-                    </label>
+        <form ref={form} onSubmit={sendEmail} className="fform">
+          <div className="fflex mt-2">
+            <label>
+              <input
+                required
+                type="text"
+                name="customerName"
+                className="iinput customername_input w-full"
+                value={customerName}
+                onChange={(e) => setCustomerName(e.target.value)}
+              />
+              <span>Username</span>
+            </label>
+          </div>
 
-                </div>  
-                        
-                <label>
-                    <input required  name="phoneNumber" className="iinput"/>
-                    <span>Phone number</span>
-                </label> 
-                    
-                <label className='messages_span'>
-                    <textarea required name='address' className="iinput01"></textarea>
-                    <span>Address</span>
-                </label>
-                    
-                <label className='messages_span'>
-                    <textarea name='description'  placeholder="Any additional messages (optional)" className="iinput01"></textarea>
-                    <span>Just say it!</span>
-                </label>
+          <label>
+            <input
+              required
+              name="phoneNumber"
+              className="iinput"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+            />
+            <span>Phone number</span>
+          </label>
+
+          <label className="messages_span">
+            <textarea
+              required
+              name="address"
+              className="iinput01"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+            ></textarea>
+            <span>Address</span>
+          </label>
+
+          <label className="messages_span">
+            <textarea name="description" placeholder="You wanna say somthin' ? feel free..." value={description} onChange={(e) => setdescription(e.target.value)} className="iinput01"></textarea>
+            <span>Just say it!</span>
+          </label>
+         
+
+          {/* Display cart items in the form */} 
+          {items.map((item, index) => (
+            <div key={index} className="fflex mt-2">
+              <label>
+                <input type="text" name={`cartItems`} className="iinput w-full text-gray-400 bg-gray-50" readOnly value={`${item.name} - ${item.price}`} />
                 
-                <button className="ffancy">
-                    <span className="ttop-key"></span>
-                    <input type="submit" value="Place order now" className='text-gray-700 text-xl font-semibold'/>
-                    <span className="bbottom-key-1"></span>
-                    <span className="bbottom-key-2"></span>
-                </button>
-            </form>
+              </label>
+            </div>
+          ))}
+
+{/* TODO:  Before placing the order do a confirmation alert */}
+          <button className="ffancy">
+            <span className="ttop-key"></span>
+            <input type="submit" value="Place order now" className="text-gray-700 text-xl font-semibold" />
+            <span className="bbottom-key-1"></span>
+            <span className="bbottom-key-2"></span>
+          </button>
+        </form>
       </div>
     </div>
   );
